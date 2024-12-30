c65680c20429d73d28ec13984cf6e593c373cee5
Ryan Bamford <ryan.bamford@aciss.com>
2024-12-30 18:07:27 -0500

    trying again

--- MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+30a09db63e5871ae9d21320fe61e8f2b25dba1a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe34039a5a2528244fc532f0727515e38dc0cad3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
