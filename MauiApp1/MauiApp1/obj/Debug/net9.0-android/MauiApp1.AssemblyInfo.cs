0e1621a6da0ef73a842b65933c079eb91bb13030
Ryan Bamford <ryan.bamford@aciss.com>
2025-01-21 17:19:27 -0500

    WIP .NET MAUI app learning

--- MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/MauiApp1/obj/Debug/net9.0-android/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+de63f245e46e4c5f71483368f29f558b4ce51150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d4a3ce5001d4c8dfc406ff10018395418ee6c34b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
